282e65bb2f5015910c7463645531be5756d0e1ba
Рамиль Куватов <b2r2@bk.ru>
2021-05-10 22:05:36 +0300

    HW3 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,99 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrEmptyPath             = errors.New("empty path")
+	ErrEmptyToPath           = errors.New("the path must not be empty")
+	ErrEqualPaths            = errors.New("from path should not be the same as to path")
+	ErrNegativeLimit         = errors.New("limit must not be less than zero")
+	ErrNegativeOffset        = errors.New("offset must not be less than zero")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	if err := checkValid(fromPath, toPath, offset, limit); err != nil {
+		return err
+	}
+
+	i, err := os.Stat(fromPath)
+	if err != nil {
+		return err
+	}
+	size := i.Size()
+	if limit == 0 || limit > size-offset {
+		limit = size - offset
+	}
+
+	var r, w *os.File
+	if r, err = os.OpenFile(fromPath, os.O_RDONLY, 0o600); err != nil {
+		fmt.Println("open")
+		return err
+	}
+	defer syncClose(r)
+	if w, err = os.Create(toPath); err != nil {
+		if err := os.Mkdir(path.Dir(toPath), os.ModePerm); err != nil {
+			return err
+		}
+		w, _ = os.Create(toPath)
+	}
+
+	b := pb.Start64(limit)
+	br := b.NewProxyReader(r)
+	defer b.Finish()
+	if _, err := io.CopyN(w, br, limit); err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func checkValid(fromPath, toPath string, offset, limit int64) error {
+	if fromPath == "" {
+		return ErrEmptyPath
+	}
+	if toPath == "" {
+		return ErrEmptyToPath
+	}
+	if fromPath == toPath {
+		return ErrEqualPaths
+	}
+	if limit < 0 {
+		return ErrNegativeLimit
+	}
+	if offset < 0 {
+		return ErrNegativeOffset
+	}
+
+	i, err := os.Stat(fromPath)
+	if err != nil {
+		return err
+	}
+	size := i.Size()
+	if size == 0 || i.IsDir() {
+		return ErrUnsupportedFile
+	}
+
+	if offset > size {
+		return ErrOffsetExceedsFileSize
+	}
+	return nil
+}
+
+func syncClose(f *os.File) {
+	if err := f.Sync(); err != nil {
+		log.Fatalf(err.Error())
+	}
+	if err := f.Close(); err != nil {
+		log.Fatalf(err.Error())
+	}
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,76 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("validation", func(t *testing.T) {
+		const testdata = "./testdata/input.txt"
+		out := "out/input.txt"
+		tests := []struct {
+			name string
+			exp  error
+			act  error
+		}{
+			{
+				"empty path",
+				ErrEmptyPath,
+				checkValid("", out, 0, 0),
+			},
+			{
+				"empty to path",
+				ErrEmptyToPath,
+				checkValid(testdata, "", 0, 0),
+			},
+			{
+				"equal paths",
+				ErrEqualPaths,
+				checkValid(testdata, testdata, 0, 0),
+			},
+			{
+				"negative limit",
+				ErrNegativeLimit,
+				checkValid(testdata, out, 0, -1),
+			},
+			{
+				"negative offset",
+				ErrNegativeOffset,
+				checkValid(testdata, out, -1, 0),
+			},
+			{
+				"offset exceeds file size",
+				ErrOffsetExceedsFileSize,
+				checkValid(testdata, out, 1<<20, 0),
+			},
+			{
+				"err unsupported file",
+				ErrUnsupportedFile,
+				checkValid("/dev/urandom", out, 0, 0),
+			},
+			{
+				"valid data",
+				nil,
+				checkValid(testdata, out, 0, 0),
+			},
+		}
+		for _, ts := range tests {
+			require.Equal(t, ts.exp, ts.act, ts.name)
+		}
+	})
+	t.Run("simple copy", func(t *testing.T) {
+		testdata := "./testdata/input.txt"
+		out := "./tmp/out.txt"
+		require.Equal(t, nil, Copy(testdata, out, 0, 0))
+		require.Nil(t, os.RemoveAll(path.Dir(out)))
+	})
+
+	t.Run("invalid data ", func(t *testing.T) {
+		testdata := "./testdata/input.txt"
+		require.Error(t, ErrEqualPaths, Copy(testdata, testdata, 0, 0))
+	})
 }
